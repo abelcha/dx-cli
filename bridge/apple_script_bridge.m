545a0161591d86057ce81af532401670858eaeb9
Abel Chalier <abel.chalier@gmail.com>
2024-01-13 06:47:28 +0100

    removed command

--- bridge/apple_script_bridge.m
+++ bridge/apple_script_bridge.m
@@ -19,17 +19,8 @@ const char* executeAppleScript(const char *script) {
 
 long long getFinderItemSize(const char *path) {
       FinderApplication *finder = [SBApplication applicationWithBundleIdentifier:@"com.apple.Finder"];
-
-        // Specify the file or folder path
-        // NSString *filePath = @"Macintosh HD:Users:abelchalier:dev:cover.ai";
         NSString *ar = [NSString stringWithUTF8String:path];
         NSURL *fileURL = [NSURL fileURLWithPath:ar];
-        // NSString *hfsPath = CFBridgingRelease(CFURLCopyFileSystemPath((__bridge CFURLRef)(fileURL), kCFURLHFSPathStyle));
-
-        // Get the information for the specified file or folder
-        // FinderItem *item = [finder home][filePath];
-        // SBElementArray *selection = [[finder selection] get];
-        // FinderItem *item = [finder home];
         FinderItem *item = [[finder items] objectAtLocation: fileURL];
         return item.size;
 }
